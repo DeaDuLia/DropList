aaaa7238f3b8517c93a617c95041e6215c134adc
DeaDuLia <xxnick1411xx@gmail.com>
2026-01-06 21:49:50 +0300

    Небольшой рефакторинг, ничего критичного

--- main.js
+++ main.js
@@ -26,12 +26,12 @@ function initializeDatabase(db) {
     `);
     db.exec(`
   INSERT OR IGNORE INTO ratings (rating) VALUES
-      ('0'),
-      ('1'),
-      ('2'),
-      ('3'),
-      ('4'),
       ('5'),
+      ('4'),
+      ('3'),
+      ('2'),
+      ('1'),
+      ('0'),
       ('-1'),
       ('-2'),
       ('-3'),
